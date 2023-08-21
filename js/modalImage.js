c95bb665d5285273cf55cb75121f18f97e27312e
ZoeLiiiiu <shiyueli@andrew.cmu.edu>
2023-08-21 02:48:38 -0400

    try the image gallery function

--- js/modalImage.js
+++ js/modalImage.js
@@ -1,4 +1,14 @@
 
+
+console.log($('.myImages'),'点击事件')
+var current = ''
+$('.myImages').bind('click',function() {
+  current = $('.myImages').index(this)
+  console.log($(this).attr('alt'),'属性')
+  $('.bottom-text').html($(this).attr('alt'))
+  console.log(current,'当前第几个')
+  }
+)
 // create references to the modal...
 var modal = document.getElementById('myModal');
 // to all images
@@ -14,6 +24,23 @@ for (var i = 0; i < images.length; i++) {
   // and attach our click listener for this image.
   img.onclick = function(evt) {
     modal.style.display = "block";
+var swiper = new Swiper('.swiper-container', {
+
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  slidesPerView: 1,
+  initialSlide : current,
+  paginationClickable: true,
+  spaceBetween: 10,
+  // loop: true,
+  onSlideChangeStart: function(swiper){
+    console.log(swiper.activeIndex,'循环第几个')
+    var text = $('.myImages').eq(swiper.activeIndex)
+    $('.bottom-text').html(text.attr('alt'))
+  },
+ 
+});
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
   }
